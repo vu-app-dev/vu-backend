80720000418954fded3621a3bdbe8f81039b474c
Eyad Hazem <eyadelmorsy16@gmail.com>
2026-07-13 00:46:17 +0300

    fix: add Exclude decorator to password field in User entity

--- src/modules/app/auth-base/user/entities/user.entity.ts
+++ src/modules/app/auth-base/user/entities/user.entity.ts
@@ -3,12 +3,14 @@ import { Column, Entity, OneToMany, OneToOne } from 'typeorm';
 import { UserTypeEnum } from '../enums/user.enum';
 import { SessionEntity } from '../../session/entities/session.entity';
 import { CompanyUser } from 'src/modules/app/companies/entities/company-user.entity';
+import { Exclude } from 'class-transformer';
 
 @Entity()
 export class User extends BaseModel {
   @Column()
   email: string;
 
+  @Exclude()
   @Column()
   password: string;
 
